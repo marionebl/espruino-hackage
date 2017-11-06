72094b279ee6205e92cfafef2cfa2dfd9819e366
ZauberNerd <zaubernerd@zaubernerd.de>
2017-11-06 17:37:07 +0000

    make devices selectable

--- list.js
+++ list.js
@@ -1,5 +1,5 @@
 var LAST = 0;
-var INTERVAL = 1000;
+var INTERVAL = 2500;
 
 function name(id) {
   return 'badge-' + id
@@ -11,25 +11,39 @@ function name(id) {
 
 var STATE = {
   id: name(NRF.getAddress()),
-  devices: []
+  devices: [],
+  selected: 0
 };
 
 // Scan for badges on Button A
-setWatch(function(e) {
+setWatch(function (e) {
   scan();
 }, BTNA, {
-  repeat: true,
-  edge: 'rising'
-});
+    repeat: true,
+    edge: 'rising'
+  });
 
 // Reset Bluetooth connections on BUtton B
-setWatch(function(e) {
+setWatch(function (e) {
   NRF.sleep();
   NRF.wake();
 }, BTNB, {
-  repeat: true,
-  edge: 'rising'
-});
+    repeat: true,
+    edge: 'rising'
+  });
+
+setWatch(function (e) {
+  STATE.selected = Math.max(0, STATE.selected - 1);
+}, BTNU, {
+    repeat: true,
+    edge: 'rising'
+  });
+setWatch(function (e) {
+  STATE.selected = Math.min(STATE.devices.length - 1, STATE.selected + 1);
+}, BTND, {
+    repeat: true,
+    edge: 'rising'
+  });
 
 
 const width = g.getWidth();
@@ -37,10 +51,18 @@ const height = g.getHeight();
 
 function render() {
   g.clear();
+  g.setColor(1);
   g.drawString(STATE.id, 0, 0);
 
   STATE.devices.forEach((device, i) => {
-    g.drawString(`${name(device.id)} ${device.rssi}`, 0, i * 6 + 2 * 6);
+    const y = i * 6 + 2 * 6;
+    if (STATE.selected === i) {
+      g.fillRect(0, y - 1, width, y + 6);
+      g.setColor(0);
+    } else {
+      g.setColor(1);
+    }
+    g.drawString(`${name(device.id)} ${device.rssi}`, 1, y);
   });
 
   g.flip();
@@ -48,11 +70,13 @@ function render() {
 }
 
 function scan() {
-    STATE.scanning = true;
-    NRF.findDevices(function(devices) {
-      STATE.scanning = false;
-      STATE.devices = devices;
-    }, INTERVAL);
+  STATE.scanning = true;
+  NRF.findDevices(function (devices) {
+    STATE.scanning = false;
+    STATE.devices = devices;
+    STATE.selected = 0;
+  }, INTERVAL);
 }
 
 render();
+scan();
